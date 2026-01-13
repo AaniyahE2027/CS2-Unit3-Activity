0e69894fc13927092fa41ac9a74be28f73ee3a26
Aaniyah <aaniyah.ellis2027@gbwl.org>
2026-01-13 16:07:57 +0000

    started

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+       // CREATE 2 Pet objects with different initial values
+      Pet dog = new Pet("Cutie", "French Bulldog", 1);
+      System.out.println (dog);
+      // TEST all Pet methods (getters, toString, setters)
    }
 }
